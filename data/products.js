fe93eb2ed4c7de8dd2d8662b67f55dbd11aa8850
arun820406 <arunk820406@gmail.com>
2025-02-19 23:16:24 +0530

    product pictures are changed

--- data/products.js
+++ data/products.js
@@ -19,7 +19,7 @@ export const products = [
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     image: "images/products/blazer 2.jpeg",
-    name: "Intermediate Size Basketball",
+    name: "Peter England Men Slim Fit Blazer",
     rating: {
       stars: 4,
       count: 127,
@@ -36,7 +36,7 @@ export const products = [
   {
     id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
     image: "images/products/blazer 3.jpeg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    name: "Van Heusen-Blazer",
     rating: {
       stars: 4.5,
       count: 56,
@@ -55,7 +55,7 @@ export const products = [
   {
     id: "54e0eccd-8f36-462b-b68a-8182611d9add",
     image: "images/products/blazer 4.jpeg",
-    name: "2 Slot Toaster - Black",
+    name: "Van Heusen-Blazer",
     rating: {
       stars: 5,
       count: 2197,
@@ -72,7 +72,7 @@ export const products = [
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
     image: "images/products/blazer 5.jpeg",
-    name: "6 Piece White Dinner Plate Set",
+    name: "Raymond-Blazer",
     rating: {
       stars: 4,
       count: 37,
@@ -89,7 +89,7 @@ export const products = [
   {
     id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
     image: "images/products/blazer 6.jpeg",
-    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    name: "Raymond-Blazer",
     rating: {
       stars: 4.5,
       count: 175,
@@ -106,7 +106,7 @@ export const products = [
   {
     id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
     image: "images/products/blazer 7.jpeg",
-    name: "Plain Hooded Fleece Sweatshirt",
+    name: "Jack & Jones-Blazer",
     rating: {
       stars: 4.5,
       count: 317,
@@ -123,7 +123,7 @@ export const products = [
   {
     id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
     image: "images/products/blazer 8.jpeg",
-    name: "Luxury Towel Set - Graphite Gray",
+    name: "Jack & Jones-Blazer",
     rating: {
       stars: 4.5,
       count: 144,
@@ -140,7 +140,7 @@ export const products = [
   {
     id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
     image: "images/products/blazer 9.jpeg",
-    name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
+    name: "Brooks Brothers-Blazer",
     rating: {
       stars: 4.5,
       count: 305,
@@ -157,7 +157,7 @@ export const products = [
   {
     id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
     image: "images/products/blazer 10.jpeg",
-    name: "Waterproof Knit Athletic Sneakers - Gray",
+    name: "Brooks Brothers-Blazer",
     rating: {
       stars: 4,
       count: 89,
@@ -174,7 +174,7 @@ export const products = [
   {
     id: "5968897c-4d27-4872-89f6-5bcb052746d7",
     image: "images/products/blazer 11.jpeg",
-    name: "Women's Chiffon Beachwear Cover Up - Black",
+    name: "Raymond-Blazer",
     rating: {
       stars: 4.5,
       count: 235,
@@ -193,7 +193,7 @@ export const products = [
   {
     id: "aad29d11-ea98-41ee-9285-b916638cac4a",
     image: "images/products/blazer 12.jpeg",
-    name: "Round Sunglasses",
+    name: "Raymond-Blazer+Pant",
     rating: {
       stars: 4.5,
       count: 30,
@@ -209,8 +209,8 @@ export const products = [
   },
   {
     id: "04701903-bc79-49c6-bc11-1af7e3651358",
-    image: "images/products/women-beach-sandals.jpg",
-    name: "Women's Two Strap Buckle Sandals - Tan",
+    image: "images/products/ashirt 1.jpeg",
+    name: "Allen Solly White Slim fit shirt",
     rating: {
       stars: 4.5,
       count: 562,
@@ -220,8 +220,8 @@ export const products = [
   },
   {
     id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
-    image: "images/products/blackout-curtain-set-beige.webp",
-    name: "Blackout Curtains Set 4-Pack - Beige",
+    image: "images/products/ashirt2.jpeg",
+    name: "Allen Solly White shirt",
     rating: {
       stars: 4.5,
       count: 232,
@@ -231,8 +231,8 @@ export const products = [
   },
   {
     id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
-    image: "images/products/men-slim-fit-summer-shorts-gray.jpg",
-    name: "Men's Slim-Fit Summer Shorts",
+    image: "images/products/ashirt 3.jpeg",
+    name: "Allen Solly Jean Shirt",
     rating: {
       stars: 4,
       count: 160,
@@ -242,8 +242,8 @@ export const products = [
   },
   {
     id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
-    image: "images/products/electric-glass-and-steel-hot-water-kettle.webp",
-    name: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
+    image: "images/products/ashirt 4.jpeg",
+    name: "Allen Solly Olive Green Shirt",
     rating: {
       stars: 5,
       count: 846,
@@ -253,8 +253,8 @@ export const products = [
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
-    image: "images/products/facial-tissue-2-ply-18-boxes.jpg",
-    name: "Ultra Soft Tissue 2-Ply - 18 Box",
+    image: "images/products/atshirt 1.jpeg",
+    name: "Allen Solly Casual T-Shirts",
     rating: {
       stars: 4,
       count: 99,
@@ -264,8 +264,8 @@ export const products = [
   },
   {
     id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
-    image: "images/products/straw-sunhat.webp",
-    name: "Straw Lifeguard Sun Hat",
+    image: "images/products/atshirt 2.jpeg",
+    name: "Allen Solly Casual T-Shirts",
     rating: {
       stars: 4,
       count: 215,
@@ -275,8 +275,8 @@ export const products = [
   },
   {
     id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
-    image: "images/products/sky-flower-stud-earrings.webp",
-    name: "Sterling Silver Sky Flower Stud Earrings",
+    image: "images/products/atshirt 2.jpeg",
+    name: "Allen Solly Casual T-Shirts(Pack-2)",
     rating: {
       stars: 4.5,
       count: 52,
@@ -286,8 +286,8 @@ export const products = [
   },
   {
     id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
-    image: "images/products/women-stretch-popover-hoodie-black.jpg",
-    name: "Women's Stretch Popover Hoodie",
+    image: "images/products/pant 1.jpeg",
+    name: "Mens Casual Pant",
     rating: {
       stars: 4.5,
       count: 2465,
@@ -299,8 +299,8 @@ export const products = [
   },
   {
     id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
-    image: "images/products/bathroom-rug.jpg",
-    name: "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
+    image: "images/products/pant 2.jpeg",
+    name: "Allen Solly Jerkin for Men's",
     rating: {
       stars: 4.5,
       count: 119,
@@ -310,8 +310,8 @@ export const products = [
   },
   {
     id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
-    image: "images/products/women-knit-ballet-flat-black.jpg",
-    name: "Women's Knit Ballet Flat",
+    image: "images/products/pant 3.jpeg",
+    name: "Men's Slim Fit Pant ",
     rating: {
       stars: 4,
       count: 326,
@@ -321,8 +321,8 @@ export const products = [
   },
   {
     id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
-    image: "images/products/men-golf-polo-t-shirt-blue.jpg",
-    name: "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
+    image: "images/products/pant 4.jpeg",
+    name: "Men's Slim Fit Pant(Olive Green)",
     rating: {
       stars: 4.5,
       count: 2556,
@@ -334,8 +334,8 @@ export const products = [
   },
   {
     id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
-    image: "images/products/trash-can-with-foot-pedal-50-liter.jpg",
-    name: "Trash Can with Foot Pedal - Brushed Stainless Steel",
+    image: "images/products/pant 5.jpeg",
+    name: "Men's Casual Pant",
     rating: {
       stars: 4.5,
       count: 2286,
@@ -345,8 +345,8 @@ export const products = [
   },
   {
     id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
-    image: "images/products/duvet-cover-set-blue-twin.jpg",
-    name: "Duvet Cover Set with Zipper Closure",
+    image: "images/products/pant 6.jpeg",
+    name: "Men's Formal Pant",
     rating: {
       stars: 4,
       count: 456,
@@ -356,8 +356,8 @@ export const products = [
   },
   {
     id: "d2785924-743d-49b3-8f03-ec258e640503",
-    image: "images/products/women-chunky-beanie-gray.webp",
-    name: "Women's Chunky Cable Beanie - Gray",
+    image: "images/products/shirt 1.jpeg",
+    name: "Formal Shirt",
     rating: {
       stars: 5,
       count: 83,
@@ -367,8 +367,8 @@ export const products = [
   },
   {
     id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
-    image: "images/products/men-chino-pants-beige.jpg",
-    name: "Men's Classic-fit Pleated Chino Pants",
+    image: "images/products/shirt 2.jpeg",
+    name: "Men's Checked Shirt",
     rating: {
       stars: 4.5,
       count: 9017,
@@ -378,8 +378,8 @@ export const products = [
   },
   {
     id: "1c079479-8586-494f-ab53-219325432536",
-    image: "images/products/men-athletic-shoes-green.jpg",
-    name: "Men's Athletic Sneaker",
+    image: "images/products/combo 1.jpeg",
+    name: "Men's Combo Shirt+Pant",
     rating: {
       stars: 4,
       count: 229,
@@ -389,8 +389,8 @@ export const products = [
   },
   {
     id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
-    image: "images/products/men-navigator-sunglasses-brown.jpg",
-    name: "Men's Navigator Sunglasses Pilot",
+    image: "images/products/combo 2.jpeg",
+    name: "Men's Combo Formal Shirt(Pack of 4)",
     rating: {
       stars: 3.5,
       count: 42,
@@ -400,8 +400,8 @@ export const products = [
   },
   {
     id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
-    image: "images/products/non-stick-cooking-set-15-pieces.webp",
-    name: "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
+    image: "images/products/combo 3.jpeg",
+    name: "Men's Combo Formal Shirt(Pack of 2)",
     rating: {
       stars: 4.5,
       count: 511,
@@ -411,8 +411,8 @@ export const products = [
   },
   {
     id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
-    image: "images/products/vanity-mirror-silver.jpg",
-    name: "Vanity Mirror with Heavy Base - Chrome",
+    image: "images/products/combo 4.jpeg",
+    name: "Men's Casual T-shirt and Jean",
     rating: {
       stars: 4.5,
       count: 130,
@@ -422,8 +422,8 @@ export const products = [
   },
   {
     id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
-    image: "images/products/women-french-terry-fleece-jogger-camo.jpg",
-    name: "Women's Fleece Jogger Sweatpant",
+    image: "images/products/combo 5.jpeg",
+    name: "Men's Formal Shirt And Pant",
     rating: {
       stars: 4.5,
       count: 248,
@@ -433,8 +433,8 @@ export const products = [
   },
   {
     id: "d339adf3-e004-4c20-a120-40e8874c66cb",
-    image: "images/products/double-elongated-twist-french-wire-earrings.webp",
-    name: "Double Oval Twist French Wire Earrings - Gold",
+    image: "images/products/combo 6.jpeg",
+    name: "Men's Formal Shirt And Pant",
     rating: {
       stars: 4.5,
       count: 117,
@@ -444,8 +444,8 @@ export const products = [
   },
   {
     id: "d37a651a-d501-483b-aae6-a9659b0757a0",
-    image: "images/products/round-airtight-food-storage-containers.jpg",
-    name: "Round Airtight Food Storage Containers - 5 Piece",
+    image: "images/products/combo 7.jpeg",
+    name: "Men's Checked Shirt And Pant",
     rating: {
       stars: 4,
       count: 126,
@@ -455,8 +455,8 @@ export const products = [
   },
   {
     id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
-    image: "images/products/coffeemaker-with-glass-carafe-black.jpg",
-    name: "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
+    image: "images/products/combo 8.jpeg",
+    name: "Men's Formal Shirt And Pant",
     rating: {
       stars: 4.5,
       count: 1211,
@@ -466,113 +466,13 @@ export const products = [
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
-    image: "images/products/blackout-curtains-black.jpg",
-    name: "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
+    image: "images/products/combo 9.jpeg",
+    name: "Men's Combo T-Shirt(Pack of 3)",
     rating: {
       stars: 4.5,
       count: 363,
     },
     priceCents: 3099,
     keywords: ["bedroom", "home"],
-  },
-  {
-    id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
-    image: "images/products/cotton-bath-towels-teal.webp",
-    name: "100% Cotton Bath Towels - 2 Pack, Light Teal",
-    rating: {
-      stars: 4.5,
-      count: 93,
-    },
-    priceCents: 2110,
-    keywords: ["bathroom", "home", "towels"],
-  },
-  {
-    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
-    image: "images/products/knit-athletic-sneakers-pink.webp",
-    name: "Waterproof Knit Athletic Sneakers - Pink",
-    rating: {
-      stars: 4,
-      count: 89,
-    },
-    priceCents: 3390,
-    keywords: ["shoes", "running shoes", "footwear", "womens"],
-  },
-  {
-    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
-    image: "images/products/countertop-blender-64-oz.jpg",
-    name: "Countertop Blender - 64oz, 1400 Watts",
-    rating: {
-      stars: 4,
-      count: 3,
-    },
-    priceCents: 10747,
-    keywords: ["food blenders", "kitchen", "appliances"],
-  },
-  {
-    id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
-    image: "images/products/floral-mixing-bowl-set.jpg",
-    name: "10-Piece Mixing Bowl Set with Lids - Floral",
-    rating: {
-      stars: 5,
-      count: 679,
-    },
-    priceCents: 3899,
-    keywords: ["mixing bowls", "baking", "cookware", "kitchen"],
-  },
-  {
-    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
-    image: "images/products/kitchen-paper-towels-30-pack.jpg",
-    name: "2-Ply Kitchen Paper Towels - 30 Pack",
-    rating: {
-      stars: 4.5,
-      count: 1045,
-    },
-    priceCents: 5799,
-    keywords: ["kitchen", "kitchen towels", "tissues"],
-  },
-  {
-    id: "bc2847e9-5323-403f-b7cf-57fde044a955",
-    image: "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
-    name: "Men's Full-Zip Hooded Fleece Sweatshirt",
-    rating: {
-      stars: 4.5,
-      count: 3157,
-    },
-    priceCents: 2400,
-    keywords: ["sweaters", "hoodies", "apparel", "mens"],
-  },
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/Blazer Masculino 1.jpeg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 10900,
-    keywords: [
-      "Blazer",
-      "blazer",
-      "Men's formal",
-      "Allen solly",
-      "Peter england",
-    ],
-  },
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/Blazer Masculino 1.jpeg",
-    name: "Peter England Men Slim Fit Blazer",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 10900,
-    keywords: [
-      "Blazer",
-      "blazer",
-      "Men's formal",
-      "Allen solly",
-      "Peter england",
-    ],
   },
 ];
